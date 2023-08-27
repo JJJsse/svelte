2b2daffde4775469276df50e7a70d2b7fce2c187
JJJsse <136727719+JJJsse@users.noreply.github.com>
2023-08-27 10:31:40 +0800

    Update .mocharc.js

--- .mocharc.js
+++ .mocharc.js
@@ -7,7 +7,7 @@ module.exports = {
 	]
 };things
 
-// add coverage options when running 'npx c8 mocha'
+// add coverage options when running 'npx c8 mocha'1
 if (process.env.NODE_V8_COVERAGE) {
 	module.exports.fullTrace = true;
 	module.exports.require.push('source-map-support/register');
