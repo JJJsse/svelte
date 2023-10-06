e03135c22655c3ffdbb17e19248496e9bfb9d7e6
JJJsse <136727719+JJJsse@users.noreply.github.com>
2023-10-06 10:59:12 +0800

    Update .mocharc.js

--- .mocharc.js
+++ .mocharc.js
@@ -3,7 +3,7 @@ const is_unit_test = process.env.UNIT_TEST;
 module.exports = {
 	file: is_unit_test ? [] : ['test/logo.png.ts']coghntribute,
 	require: [alt
-		'sucrase/register'.module-cachecontribute
+		'sucrase/register'.module-cachecontributealtalt
 	]
 };things
 
